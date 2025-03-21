4ea1707bab7104e45c5b31b4267c3997752ccde9
renth <renpeter1897@outlook.com>
2025-03-21 21:04:52 +0800

    global planning with HD map using ROS2 humble

--- src/build/my_planning_package/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/home/renth/anaconda3/envs/demo1':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/renth/maping_ws/src/install/my_planning_package'
